066f49b296546e0ee9c9407a14db0b11de455cc7
Victor Manuel Organista Hernandez <493+vorganista@users.noreply.github.palaceresorts.com>
2025-02-28 16:08:57 -0600

    prueba

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <div>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+        alt=""
+        class="brighness-0"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,18 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h1 class="animate-pulse">Cargando Pokemons</h1>
+  </section>
+
+  <section class="flex flex-col justify-center items-ceter w-screen h-screen">
+    <h1>¿Quien es este Pokemon?</h1>
+    <!--Pokemon Picture-->
+    <PokemonPicture />
+    <!--pokemon option-->
+    <!---->
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonPicture from '../components/PokemonPicture.vue';
+</script>
